64e0b071927c022414ea182707d24d9a825b05b3
kiterumer <1404646092@qq.com>
2019-05-01 17:00:36 +0800

    使用chai进行测试输入参数，点击测试没有

--- src/app.js
+++ src/app.js
@@ -15,3 +15,84 @@ new Vue({
         loading3:false
     }
 })
+
+import chai from 'chai'
+const expect = chai.expect
+//单元测试  
+//没有消息就是好消息
+{
+    console.log(Button)  //Button是一个对象，而我们需要使用构造函数
+    //动态生成一个按钮
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings'  //用户输入settings配置
+        }
+    })
+    vm.$mount()
+    console.log(vm)
+    console.log(vm.$el)
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')  //期望use的href是#i-settings
+
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings',
+            loading: true  
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'settings',
+            iconPosition:'right'  //测试 right
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')  //css属性值是字符串
+    //测试完清理
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    //mock
+    const Constructor = Vue.extend(Button)
+    const vm = new  Constructor({
+        propsData:{
+            icon:'settings',
+            loading: true  
+        }
+    })
+    vm.$mount()
+    vm.$on('click',function(){  //希望这个被执行
+        console.log(1)
+        expect(1).to.eq(1)
+    })
+    let button = vm.$el
+    button.click()
+
+    vm.$el.remove()
+    vm.$destroy()
+}

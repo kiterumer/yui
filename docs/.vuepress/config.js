e02a56fb5d8e4a6028618103de664ab8cab9a727
kiterumer <1404646092@qq.com>
2019-05-17 12:39:19 +0800

    使用vuepress制作UI官网，可以在本地预览

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    title: 'Young UI',
+    description: '一个简洁好用的UI框架',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+          ],
+        sidebar: [
+          '/',
+          {
+              title:'入门',
+              children:['/install/','/get-started/']
+          },
+          {
+              title:'组件',
+              children:['/components/button']
+          }
+        ]
+      }
+  }
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# Young UI 官网
+
+Young UI 是一个基于Vue的简洁的、易上手的UI组件库。
